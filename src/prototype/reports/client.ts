5af30167c8b5740523b820e8a463756f1286f381
macluiggy <lmacias@viamatica.com>
2025-02-10 13:36:20 -0500

    Add elements property to GraphicReport and enhance cloning demonstration

--- src/prototype/reports/client.ts
+++ src/prototype/reports/client.ts
@@ -4,6 +4,7 @@ const report = new GraphicReport();
 report.title = "Graphic Report";
 report.date = new Date();
 report.graphic = "Some graphic content";
+report.elements = ["Element 1", "Element 2", "Element 3"];
 const clonedReport = report.clone();
 clonedReport.title = "Cloned Graphic Report";
 clonedReport.date = new Date();
@@ -11,3 +12,6 @@ clonedReport.graphic = "Some cloned graphic content";
 
 console.log(report);
 console.log(clonedReport);
+
+const elementsAreEqual = report.elements === clonedReport.elements;
+console.log(`Report elements and cloned report elements are the same: ${elementsAreEqual}`);
--- src/prototype/reports/graphic-report.ts
+++ src/prototype/reports/graphic-report.ts
@@ -4,6 +4,9 @@ export class GraphicReport implements Report {
   public title: string;
   public date: Date;
   public graphic: string;
+  // Added an array property called "elements"
+  public elements: any[];
+
   constructor(sourceOrTitle?: GraphicReport) {
     if (!sourceOrTitle) {
       return;
@@ -11,7 +14,10 @@ export class GraphicReport implements Report {
     this.title = sourceOrTitle.title;
     this.date = sourceOrTitle.date;
     this.graphic = sourceOrTitle.graphic;
+    // Copy array logic: creates a shallow copy of the elements array
+    this.elements = sourceOrTitle.elements ? [...sourceOrTitle.elements] : [];
   }
+
   public clone(): GraphicReport {
     return new GraphicReport(this);
   }
